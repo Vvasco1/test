4c5353a0d96af0dc64ec374bd773d571a7bf68ed
Vvasco1 <109784466+Vvasco1@users.noreply.github.com>
2023-09-20 00:36:31 +0900

    Update output.tf

--- network/output.tf
+++ network/output.tf
@@ -1,11 +1,11 @@
-output "vpc name" {
+output "vpc_id" {
 	value = aws_vpc.test.id
 }
 
-output "NAT GW" {
+output "nat_gw" {
 	value = aws_nat_gateway.natgw
 }
 
-out "subnet public a" {
+out "subnet_public_a" {
 	value = aws_subnet.pub_a.id
 }

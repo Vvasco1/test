21f4d224f748b9b035c2a623b7b65ffa730bc535
Vvasco1 <109784466+Vvasco1@users.noreply.github.com>
2023-09-19 22:01:37 +0900

    Update bastion.tf

--- ec2/bastion.tf
+++ ec2/bastion.tf
@@ -1,9 +1,16 @@
+data "aws_vpc" "existing_vpc" { #다른 워크스페이스에 있는 VPC 정보를 가져오기 위함
+  filter {
+    name   = "tag:Name"
+    values = ["TEST-VPC"] # 생성된 VPC 중 사용할 VPC 이름
+  }
+}
+
 resource "aws_instance" "bastion" {
   ami                         = "ami-0c76973fbe0ee100c" # Amazon Linux 2
-  instance_type               = "t2.micro" 
-  vpc_security_group_ids      = [aws_security_group.bastion.id] # 보안 그룹 종속
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
   subnet_id = aws_subnet.pub_a.id
-  depends_on                  = [aws_nat_gateway.natgw] # NAT 구성 후 생성
+  depends_on                  = [aws_nat_gateway.natgw]
   tags = {
     Name = "Bastion"
   }
@@ -11,7 +18,7 @@ resource "aws_instance" "bastion" {
 resource "aws_security_group" "bastion" {
   name        = "Bastion-SG"
   description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.test.id
+  vpc_id      = data.aws_vpc.existing_vpc.id
   ingress {
     description = "SSH from VPC"
     from_port   = 22
